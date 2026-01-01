3152fe6f4049dac3e29043869b5bef7ecb00ee14
Ehsan <dev2024loper@gmail.com>
2026-01-01 13:41:22 +0330

    Add password test

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -4,3 +4,5 @@
 def register_user(email, password):
     if "@" not in email:
         raise ValueError('Invalid email')
+    if len(password) < 8:
+        raise ValueError('Password is too short')
--- tests/test_user_registration.py
+++ tests/test_user_registration.py
@@ -1,7 +1,13 @@
+import sys
+sys.path.append('.')
+
 import pytest
 from app.services.user_service import register_user
-
 
 def test_register_user_with_invalid_email_should_fail():
     with pytest.raises(ValueError):
         register_user('invalid-email', '123456789')
+
+def test_register_user_with_invalid_password_should_fail():
+    with pytest.raises(ValueError):
+        register_user('valid@emial', '123')        
