ccb7e066fdf38961982a4a99679fc7daa33a3a45
Ehsan <dev2024loper@gmail.com>
2026-01-01 19:09:31 +0330

    inject dependecy FakeUserRepository and emil exsistence test

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -1,8 +1,12 @@
 
 
 
-def register_user(email, password):
+def register_user(email, password, user_repository):
     if "@" not in email:
         raise ValueError('Invalid email')
+    
     if len(password) < 8:
         raise ValueError('Password is too short')
+    
+    if user_repository.email_exists(email):
+        raise ValueError('Email already exists')
--- tests/test_user_registration.py
+++ tests/test_user_registration.py
@@ -4,10 +4,20 @@ sys.path.append('.')
 import pytest
 from app.services.user_service import register_user
 
+class FakeUserRepository:
+    def email_exists(self, email):
+        return True
+    
+repo = FakeUserRepository()
+
 def test_register_user_with_invalid_email_should_fail():
     with pytest.raises(ValueError):
-        register_user('invalid-email', '123456789')
+        register_user('invalid-email', '123456789', repo)
 
 def test_register_user_with_invalid_password_should_fail():
     with pytest.raises(ValueError):
-        register_user('valid@emial', '123')        
+        register_user('valid@emial', '123', repo)
+
+def test_register_user_with_exists_email_should_fail():
+    with pytest.raises(ValueError):
+        register_user('user@test.com', '12345678', repo)
