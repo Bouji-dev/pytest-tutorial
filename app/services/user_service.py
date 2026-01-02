1e5e5064cfd349cf883c1d42bb823df7abfa8c78
Ehsan <dev2024loper@gmail.com>
2026-01-02 17:55:34 +0330

    Refactor rgister_user

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -1,9 +1,19 @@
-def register_user(email, password, user_repository):
-    if "@" not in email:
-        raise ValueError('Invalid email')
+
+def validate_email(email):
+    if not email or "@" not in email:
+        raise ValueError("Invalid email")
     
+def validate_password(password):
     if len(password) < 8:
         raise ValueError('Password is too short')
-    
+
+def validate_email_exists(email, user_repository):
     if user_repository.email_exists(email):
         raise ValueError('Email already exists')
+
+def register_user(email, password, user_repository):
+    validate_email(email)
+    validate_password(password)
+    validate_email_exists(email, user_repository)
+    
+
